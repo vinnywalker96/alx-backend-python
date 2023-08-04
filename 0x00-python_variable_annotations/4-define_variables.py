549f4c4ef926f0cfa6fcb806ff65ee1472334571
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-04 16:12:04 +0200

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic annotation -- Defining Variables"""
+
+import math
+a: int = 1
+pi: float = math.pi
+i_understand_annotations: bool = True
+school: str = "Holberton"
