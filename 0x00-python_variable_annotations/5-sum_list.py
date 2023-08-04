02a6901b761b2bf81ffee9be042aabd8b85f909a
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-04 16:17:50 +0200

    sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Complex types - list of floats """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Return the sum of a list of floats """
+    return sum(input_list)
