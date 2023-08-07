c2e19ffea2a4092f3daa89d53cd5085523eb4d67
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-07 11:03:56 +0200

    make multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,4 +4,12 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """_summary_
+
+    Args:
+        multiplier (float): _description_
+
+    Returns:
+        Callable[[float], float]: _description_
+    """
     return lambda y: multiplier * y
