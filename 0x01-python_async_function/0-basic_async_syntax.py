6e227811eb4f77986d7b81d51aabd746d5aea1ce
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-09 14:04:18 +0200

    basic async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -19,7 +19,7 @@ async def wait_random(max_delay: int = 10) -> float:
     await asyncio.sleep(i)
     time_elapsed = time.perf_counter() - start
 
-    return f'{time_elapsed}'
+    return f'{time_elapsed:0.15f}'
 
 
 print(asyncio.run(wait_random()))
