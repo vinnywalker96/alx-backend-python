8b9b09ce1d05e9ff74d35bc611298a9cf4e2da0b
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-10 15:28:52 +0200

    concurrent coroutine

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 """execute multiple coroutines at the same time with async"""
 import asyncio
-import time
-import random
 from typing import List
-
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
