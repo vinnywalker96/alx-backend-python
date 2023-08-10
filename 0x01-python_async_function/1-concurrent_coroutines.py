76d37631d252c50c13bea5410c2ecf1cfe51d3b9
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-10 14:13:05 +0200

    concurrent coroutine

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,16 +3,13 @@
 import asyncio
 import time
 import random
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
-    list_delay: list[float] = []
-    start = time.perf_counter()
-    i = random.uniform(0, n)
-    await asyncio.sleep(i)
-    time_elapsed = time.perf_counter() - start
-    list_delay.append(time_elapsed)
-    return list_delay
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
