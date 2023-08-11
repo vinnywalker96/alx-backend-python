ed0253ddf9bfc52ba08cf6dc63dd6e0a65c3e18b
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-11 18:34:27 +0200

    async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Async Compression"""
 import asyncio
-from typing import AsyncGenerator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> AsyncGenerator[float, None]:
+async def async_comprehension() -> List[float]:
     """_summary_
 
     Returns:
