8864dd255d8376692de8c43c101f9a2617022e59
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-11 08:55:40 +0200

    tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Tasks"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """_summary_
+
+    Args:
+        max_delay (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(wait_random(max_delay))
+    return task
