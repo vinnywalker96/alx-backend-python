1b72b0bbc30a00d207ee830a1b3659d3011ce071
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-12 16:42:03 +0200

    parameterized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,8 @@
 import unittest
 from utils import (
         access_nested_map,
-        get_json
+        get_json,
+        memoize
         )
 from unittest.mock import patch, Mock
 from parameterized import parameterized
@@ -50,6 +51,27 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**args)) as req:
             self.assertEqual(get_json(test_url), test_payload)
             req.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Parameterize a patch"""
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
 
 
 if __name__ == "__main__":
