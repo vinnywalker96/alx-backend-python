0b1b4b85d2ee54974d3ce2c5989cbb6c72a30543
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-13 17:51:25 +0200

    Property Mock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -46,6 +46,68 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/users/google/repos",
             )
 
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """Tests the `public_repos` method."""
+        test_payload = {
+                'repos_url': 'https://api.github.com/orgs/google/repos', 
+	            'repo': [
+                    {
+                        'login': 'google',
+	                    'id': 1342004,
+
+	                },
+                    {
+                        'node_id': 'MDEyOk9yZ2FuaXphdGlvbjEzNDIwMDQ=',
+                        'url': 'https://api.github.com/orgs/google',
+                        'repos_url': 'https://api.github.com/orgs/google/repos',
+                        'events_url': 'https://api.github.com/orgs/google/events',
+                        'hooks_url': 'https://api.github.com/orgs/google/hooks', 
+                        'issues_url': 'https://api.github.com/orgs/google/issues',
+                        'members_url': 'https://api.github.com/orgs/google/members{/member}',
+                    },
+                    {
+                        'name': 'Google',
+                        'company': None, 
+                        'blog': 'https://opensource.google/',
+                        'location': None, 
+                        'email': 'opensource@google.com', 
+                        'twitter_username': 'GoogleOSS', 
+                        'is_verified': True,
+                        'has_organization_projects': True, 
+                        'has_repository_projects': True, 
+                        'public_repos': 2552, 
+                        'public_gists': 0, 
+                        'followers': 28884,
+                        'following': 0, 
+                        'html_url': 'https://github.com/google', 
+                        'created_at': '2012-01-18T01:30:18Z', 
+                        'updated_at': '2021-12-30T01:40:20Z', 
+                        'archived_at': None, 'type': 'Organization'
+                    },
+                    ]
+                }
+        mock_get_json.return_value = test_payload["repo"]
+        with patch(
+                "client.GithubOrgClient._public_repos_url",
+                new_callable=PropertyMock,
+                ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = test_payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient("google").public_repos(),
+                [
+                    "users",
+                    "likes",
+                ],
+            )
+            mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
+
+
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
